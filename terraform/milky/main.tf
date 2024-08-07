0c9be53270b931b6483cef06141ba72d450e22fb
Yuta Takahashi <y.highbridge04@gmail.com>
2024-08-07 15:46:41 +0900

    feat: Update Terraform configuration for Milky Cloud deployment

--- terraform/milky/main.tf
+++ terraform/milky/main.tf
@@ -1,58 +1,58 @@
 variable "s3_bucket" {
-    type = string
+  type = string
 }
 
 variable "s3_region" {
-    type = string
+  type = string
 }
 variable "s3_endpoint" {
-    type = string
+  type = string
 }
 variable "s3_access_key" {
-    type = string
+  type = string
 }
 variable "s3_secret_key" {
-    type = string
+  type = string
 }
 variable "pm_api_url" {
-    type = string
+  type = string
 }
 variable "pm_api_token_id" {
-    type = string
+  type = string
 }
 variable "pm_api_token_secret" {
-    type = string
+  type = string
 }
 
 terraform {
-    required_providers {
-        proxmox = {
-            source = "Telmate/proxmox"
-            version = ">= 3.0.0"
-        }
+  required_providers {
+    proxmox = {
+      source  = "Telmate/proxmox"
+      version = ">= 3.0.0"
     }
-    backend "s3" {
-        # Reference From https://docs.oracle.com/en-us/iaas/Content/API/SDKDocs/terraformUsingObjectStore.htm
-        bucket = var.s3_bucket
-        key    = "milky-terraform.tfstate"
-        region = var.s3_region
-        endpoint = var.s3_endpoint
-        access_key = var.s3_access_key
-        secret_key = var.s3_secret_key
+  }
+  backend "s3" {
+    # Reference From https://docs.oracle.com/en-us/iaas/Content/API/SDKDocs/terraformUsingObjectStore.htm
+    bucket     = var.s3_bucket
+    key        = "milky-terraform.tfstate"
+    region     = var.s3_region
+    endpoint   = var.s3_endpoint
+    access_key = var.s3_access_key
+    secret_key = var.s3_secret_key
 
-        # AWS認証をスキップ
-        force_path_style = true
-        skip_region_validation = true
-        skip_credentials_validation = true
-        skip_metadata_api_check = true
-        skip_requesting_account_id = true
-        skip_get_ec2_platforms = true
-    }
+    # AWS認証をスキップ
+    force_path_style            = true
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_requesting_account_id  = true
+    skip_get_ec2_platforms      = true
+  }
 }
 
 provider "proxmox" {
-    pm_api_url = var.pm_api_url
-    pm_api_token_id = var.pm_api_token_id
-    pm_api_token_secret = var.pm_api_token_secret
+  pm_api_url          = var.pm_api_url
+  pm_api_token_id     = var.pm_api_token_id
+  pm_api_token_secret = var.pm_api_token_secret
 
 }
