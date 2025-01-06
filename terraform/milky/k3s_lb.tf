f0f668b112926971b4bcc17aea8418ffd1e0bfeb
Yuta Takahashi <y.highbridge04@gmail.com>
2025-01-06 17:58:41 +0900

    Add SSH public keys to K3s LoadBalancer LXC configurations

--- terraform/milky/k3s_lb.tf
+++ terraform/milky/k3s_lb.tf
@@ -20,7 +20,11 @@ resource "proxmox_lxc" "k3s-lb-1" {
   memory = 256
   cores  = 1
 
-  onboot = true
+  onboot          = true
+  ssh_public_keys = <<EOF
+${var.ssh_public_key}
+${var.ssh_public_key_k3s}
+  EOF
 
 
   network {
@@ -28,7 +32,7 @@ resource "proxmox_lxc" "k3s-lb-1" {
     bridge   = "k3s"
     firewall = false
     ip       = "10.100.0.5/24"
-    mtu     = 1400
+    mtu      = 1400
   }
   network {
     name     = "eth1"
@@ -46,7 +50,11 @@ resource "proxmox_lxc" "k3s-lb-2" {
   description = "Management Kubernetes Cluster LoadBalancer"
   target_node = "milky-carina"
 
-  vmid = 12002
+  ssh_public_keys = <<EOF
+${var.ssh_public_key}
+${var.ssh_public_key_k3s}
+  EOF
+  vmid            = 12002
 
   clone = 9101
 
@@ -71,7 +79,7 @@ resource "proxmox_lxc" "k3s-lb-2" {
     bridge   = "k3s"
     firewall = false
     ip       = "10.100.0.6/24"
-    mtu     = 1400
+    mtu      = 1400
   }
   network {
     name     = "eth1"
@@ -82,47 +90,7 @@ resource "proxmox_lxc" "k3s-lb-2" {
   }
 
 }
-resource "proxmox_lxc" "k3s-lb-1" {
-  hostname    = "solufit-k3s-lb-1"
-  description = "Management Kubernetes Cluster LoadBalancer"
-  target_node = "milky-capella"
 
-  vmid = 12001
-
-  clone = 9100
-
-  start = true
-
-  rootfs {
-    storage = "local-lvm"
-    size    = "8G"
-  }
-
-  # The destination resource pool for the new VM
-  pool = "solufit"
-
-  memory = 256
-  cores  = 1
-
-  onboot = true
-
-
-  network {
-    name     = "eth0"
-    bridge   = "k3s"
-    firewall = false
-    ip       = "10.100.0.5/24"
-    mtu     = 1400
-  }
-  network {
-    name     = "eth1"
-    bridge   = "vmbr2"
-    firewall = false
-    ip       = "dhcp"
-    mtu      = 1400
-  }
-
-}
 
 
 resource "proxmox_lxc" "k3s-lb-3" {
@@ -135,6 +103,11 @@ resource "proxmox_lxc" "k3s-lb-3" {
   clone = 9102
 
   start = true
+
+  ssh_public_keys = <<EOF
+${var.ssh_public_key}
+${var.ssh_public_key_k3s}
+  EOF
 
   rootfs {
     storage = "local-lvm"
@@ -155,7 +128,7 @@ resource "proxmox_lxc" "k3s-lb-3" {
     bridge   = "k3s"
     firewall = false
     ip       = "10.100.0.7/24"
-    mtu     = 1400
+    mtu      = 1400
   }
   network {
     name     = "eth1"
