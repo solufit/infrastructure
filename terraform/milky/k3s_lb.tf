a8255f560a07132e67749353c83aae6737cc9655
Yuta Takahashi <y.highbridge04@gmail.com>
2025-01-06 17:54:03 +0900

    Add Proxmox LXC configurations for K3s LoadBalancer instances

---
+++ terraform/milky/k3s_lb.tf
@@ -0,0 +1,170 @@
+resource "proxmox_lxc" "k3s-lb-1" {
+  hostname    = "solufit-k3s-lb-1"
+  description = "Management Kubernetes Cluster LoadBalancer"
+  target_node = "milky-capella"
+
+  vmid = 12001
+
+  clone = 9100
+
+  start = true
+
+  rootfs {
+    storage = "local-lvm"
+    size    = "8G"
+  }
+
+  # The destination resource pool for the new VM
+  pool = "solufit"
+
+  memory = 256
+  cores  = 1
+
+  onboot = true
+
+
+  network {
+    name     = "eth0"
+    bridge   = "k3s"
+    firewall = false
+    ip       = "10.100.0.5/24"
+    mtu     = 1400
+  }
+  network {
+    name     = "eth1"
+    bridge   = "vmbr2"
+    firewall = false
+    ip       = "dhcp"
+    mtu      = 1400
+  }
+
+}
+
+
+resource "proxmox_lxc" "k3s-lb-2" {
+  hostname    = "solufit-k3s-lb-2"
+  description = "Management Kubernetes Cluster LoadBalancer"
+  target_node = "milky-carina"
+
+  vmid = 12002
+
+  clone = 9101
+
+  start = true
+
+  rootfs {
+    storage = "local-lvm"
+    size    = "8G"
+  }
+
+  # The destination resource pool for the new VM
+  pool = "solufit"
+
+  memory = 256
+  cores  = 1
+
+  onboot = true
+
+
+  network {
+    name     = "eth0"
+    bridge   = "k3s"
+    firewall = false
+    ip       = "10.100.0.6/24"
+    mtu     = 1400
+  }
+  network {
+    name     = "eth1"
+    bridge   = "vmbr2"
+    firewall = false
+    ip       = "dhcp"
+    mtu      = 1400
+  }
+
+}
+resource "proxmox_lxc" "k3s-lb-1" {
+  hostname    = "solufit-k3s-lb-1"
+  description = "Management Kubernetes Cluster LoadBalancer"
+  target_node = "milky-capella"
+
+  vmid = 12001
+
+  clone = 9100
+
+  start = true
+
+  rootfs {
+    storage = "local-lvm"
+    size    = "8G"
+  }
+
+  # The destination resource pool for the new VM
+  pool = "solufit"
+
+  memory = 256
+  cores  = 1
+
+  onboot = true
+
+
+  network {
+    name     = "eth0"
+    bridge   = "k3s"
+    firewall = false
+    ip       = "10.100.0.5/24"
+    mtu     = 1400
+  }
+  network {
+    name     = "eth1"
+    bridge   = "vmbr2"
+    firewall = false
+    ip       = "dhcp"
+    mtu      = 1400
+  }
+
+}
+
+
+resource "proxmox_lxc" "k3s-lb-3" {
+  hostname    = "solufit-k3s-lb-3"
+  description = "Management Kubernetes Cluster LoadBalancer"
+  target_node = "milky-capella"
+
+  vmid = 12002
+
+  clone = 9102
+
+  start = true
+
+  rootfs {
+    storage = "local-lvm"
+    size    = "8G"
+  }
+
+  # The destination resource pool for the new VM
+  pool = "solufit"
+
+  memory = 256
+  cores  = 1
+
+  onboot = true
+
+
+  network {
+    name     = "eth0"
+    bridge   = "k3s"
+    firewall = false
+    ip       = "10.100.0.7/24"
+    mtu     = 1400
+  }
+  network {
+    name     = "eth1"
+    bridge   = "vmbr2"
+    firewall = false
+    ip       = "dhcp"
+    mtu      = 1400
+  }
+
+}
+
+
