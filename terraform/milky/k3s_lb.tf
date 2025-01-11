b039296ff7d3cf84a79a8b3f8389d346f7d524e8
Yuta Takahashi <y.highbridge04@gmail.com>
2025-01-11 22:03:35 +0900

    Add remote-exec provisioner to k3s LoadBalancer LXC configurations and update IP addresses

--- terraform/milky/k3s_lb.tf
+++ terraform/milky/k3s_lb.tf
@@ -14,57 +14,6 @@ resource "proxmox_lxc" "k3s-lb-1" {
     storage = "local-lvm"
     size    = "8G"
   }
-  ssh_public_keys = <<EOT
-${var.ssh_public_key}
-${var.ssh_public_key_k3s}
-  EOT
-  # The destination resource pool for the new VM
-  pool = "solufit"
-
-  memory = 256
-  cores  = 1
-
-  onboot = true
-
-
-  network {
-    name     = "eth0"
-    bridge   = "k3s"
-    firewall = false
-    ip       = "10.100.0.5/24"
-  }
-  network {
-    name     = "eth1"
-    bridge   = "vmbr2"
-    firewall = false
-    ip       = "dhcp"
-    mtu      = 1400
-  }
-
-}
-
-
-
-
-resource "proxmox_lxc" "k3s-lb-2" {
-  hostname    = "solufit-k3s-lb-2"
-  description = "cloudflare tunnel for Solufit"
-  target_node = "milky-carina"
-
-  vmid = 12002
-
-  clone = 9101
-
-  start           = true
-  ssh_public_keys = <<EOT
-${var.ssh_public_key}
-${var.ssh_public_key_k3s}
-  EOT
-  rootfs {
-    storage = "local-lvm"
-    size    = "8G"
-  }
-
   # The destination resource pool for the new VM
   pool = "solufit"
 
@@ -88,23 +37,33 @@ ${var.ssh_public_key_k3s}
     mtu      = 1400
   }
 
+  provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      user        = "root"
+      private_key = var.ssh_private_key
+      host        = "10.100.0.6"
+    }
+    inline = [
+        "echo ${var.ssh_public_key_k3s} >> /root/.ssh/authorized_keys"
+    ]
+
+}
 }
 
 
-resource "proxmox_lxc" "k3s-lb-3" {
-  hostname    = "solufit-k3s-lb-3"
+
+
+resource "proxmox_lxc" "k3s-lb-2" {
+  hostname    = "solufit-k3s-lb-2"
   description = "cloudflare tunnel for Solufit"
-  target_node = "milky-polaris"
+  target_node = "milky-carina"
 
-  vmid = 12003
+  vmid = 12002
 
-  clone = 9102
+  clone = 9101
 
-  start           = true
-  ssh_public_keys = <<EOT
-${var.ssh_public_key}
-${var.ssh_public_key_k3s}
-  EOT
+  start = true
   rootfs {
     storage = "local-lvm"
     size    = "8G"
@@ -132,5 +91,69 @@ ${var.ssh_public_key_k3s}
     ip       = "dhcp"
     mtu      = 1400
   }
+  provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      user        = "root"
+      private_key = var.ssh_private_key
+      host        = "10.100.0.7"
+    }
+    inline = [
+        "echo ${var.ssh_public_key_k3s} >> /root/.ssh/authorized_keys"
+    ]
 
+}
+}
+
+
+resource "proxmox_lxc" "k3s-lb-3" {
+  hostname    = "solufit-k3s-lb-3"
+  description = "cloudflare tunnel for Solufit"
+  target_node = "milky-polaris"
+
+  vmid = 12003
+
+  clone = 9102
+
+  start = true
+  rootfs {
+    storage = "local-lvm"
+    size    = "8G"
+  }
+
+  # The destination resource pool for the new VM
+  pool = "solufit"
+
+  memory = 256
+  cores  = 1
+
+  onboot = true
+
+
+  network {
+    name     = "eth0"
+    bridge   = "k3s"
+    firewall = false
+    ip       = "10.100.0.8/24"
+  }
+  network {
+    name     = "eth1"
+    bridge   = "vmbr2"
+    firewall = false
+    ip       = "dhcp"
+    mtu      = 1400
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      user        = "root"
+      private_key = var.ssh_private_key
+      host        = "10.100.0.8"
+    }
+    inline = [
+        "echo ${var.ssh_public_key_k3s} >> /root/.ssh/authorized_keys"
+    ]
+
+}
 }
