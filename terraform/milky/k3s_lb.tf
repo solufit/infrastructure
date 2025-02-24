e4f16af6a51101c535ea18c9fb9d819d8cb92daa
Yuta Takahashi <y.highbridge04@gmail.com>
2025-02-24 16:52:42 +0900

    Add Proxmox LXC resource for NFS with Cloudflare tunnel configuration

--- terraform/milky/k3s_lb.tf
+++ terraform/milky/k3s_lb.tf
@@ -162,3 +162,58 @@ resource "proxmox_lxc" "k3s-lb-3" {
 
   }
 }
+resource "proxmox_lxc" "k3s-nfs" {
+  hostname    = "solufit-k3s-nfs"
+  description = "cloudflare tunnel for Solufit"
+  target_node = "milky-polaris"
+
+  vmid = 12004
+
+  clone = 9102
+
+  start = true
+  rootfs {
+    storage = "main-storage"
+    size    = "8G"
+  }
+
+  # The destination resource pool for the new VM
+  pool = "solufit"
+
+  memory = 256
+  cores  = 1
+
+  onboot = true
+
+
+  network {
+    name     = "eth0"
+    bridge   = "k3s"
+    firewall = false
+    ip       = "10.100.0.9/24"
+  }
+  network {
+    name     = "eth1"
+    bridge   = "vmbr2"
+    firewall = false
+    ip       = "dhcp"
+    mtu      = 1400
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      user        = "root"
+      private_key = var.ssh_private_key
+      host        = "10.100.0.9"
+    }
+    inline = [
+      "mkdir -p /root/.ssh",
+      "echo '${var.ssh_public_key_k3s}' >> /root/.ssh/authorized_keys",
+      "chmod 700 /root/.ssh && chmod 600 /root/.ssh/authorized_keys",
+      "sudo apt-get update && sudo apt-get upgrade -y",
+      "sudo reboot"
+    ]
+
+  }
+}
